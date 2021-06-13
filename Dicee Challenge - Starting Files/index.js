8df95ae1ce45983df98d773fd64a972cf81d3702
Vartica01 <54020389+Vartica01@users.noreply.github.com>
2021-06-13 20:49:22 +0530

    Add files via upload

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,14 @@
+var randomnumber = Math.floor(Math.random()*6)+1;
+var imgsrc = "images/dice"+randomnumber+".png";
+document.querySelectorAll("img")[0].setAttribute("src",imgsrc);
+
+var randomnumber2 = Math.floor(Math.random()*6)+1;
+var imgsrc2 = "images/dice"+randomnumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",imgsrc2);
+if(randomnumber>randomnumber2){
+  document.querySelector("h1").innerHTML = "Player 1 wins!!!";
+}else if(randomnumber<randomnumber2){
+  document.querySelector("h1").innerHTML = "Player 2 wins!!!";
+}else{
+  document.querySelector("h1").innerHTML = "Draw";
+}
